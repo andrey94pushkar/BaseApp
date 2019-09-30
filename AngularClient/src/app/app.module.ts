c1f86559430615dfb6a26c8f2c82a20e1906515b
Эдуард <eduardtovstuha@gmail.com>
2019-09-30 22:26:21 +0300

    VERSTKA(profile page)

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialAppModule } from './material/ngmaterial.module';
+import { ProfileComponent } from './profile/profile.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MaterialAppModule,
+    MatGridListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class CheckboxOverviewExample {}
+export class ButtonOverviewExample {}
+
+
