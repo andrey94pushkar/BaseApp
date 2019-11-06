dfdf24aef0c28a04736378b471c395a07fde274a
Богдан Гребенюк <bodakuc21@gmail.com>
2019-11-07 00:11:05 +0200

    Added chat component

---
+++ AngularClient/src/app/shared/chat/chat.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {AutofillMonitor} from '@angular/cdk/text-field';
+import {AfterViewInit, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
